c33cb902505fdd2c92d1c67996aa5101fa987e20
Shreyas Pansare <76799724+shreyas-exe@users.noreply.github.com>
2025-02-22 13:21:55 +0530

    Reapply "'start'"
    This reverts commit 97b468652cd0b27879455cb73ec9783b29b6217f.

---
+++ Backend/Models/UserModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "venue_manager", "comedian", "customer"], required: true },
+});
+
+
+module.exports = mongoose.model("User", userSchema);
