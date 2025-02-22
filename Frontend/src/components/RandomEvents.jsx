e67eb1503dd8a01d6e0cfe29ed2414f5cac1cd42
Yash Mulik <yashmulik95@gmail.com>
2025-02-22 21:32:09 +0530

    search

--- Frontend/src/components/RandomEvents.jsx
+++ Frontend/src/components/RandomEvents.jsx
@@ -4,7 +4,14 @@ import EventCard from "./EventCard";
 function RandomEvents() {
   return (
     <div>
-      <h1 className=" text-white my-3">Upcomming Events</h1>
+      <div className=" flex flex-row justify-between items-center p-4">
+        <h1 className=" text-white my-3">Upcomming Events</h1>
+        <input
+          type="text"
+          className=" font-normal text-white p-[6px] px-4 rounded-lg bg-gray-600 w-[25%]"
+          placeholder="artist name"
+        />
+      </div>
       <div className=" grid md:grid-cols-4 gap-4 p-4">
         <EventCard />
         <EventCard />
