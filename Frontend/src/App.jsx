1ce8899d30956ddd48e1084696241aad68a6794c
shivraj0124 <shivrajkolwankar0124@gmail.com>
2025-02-22 12:16:42 +0530

    'start'

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Payment from "./components/Payment";
+
+function App() {
+  return (
+    <>
+      <div className="font-bold text-center">Hello World
+
+        <Payment/>
+      </div>
+    </>
+  );
+}
+
+export default App;
